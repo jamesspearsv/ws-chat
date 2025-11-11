1eb2b31dfda2d83fd4c91f99b58914676f33813a
James Spears <jamesspearsv@gmail.com>
2025-11-10 22:14:14 -0500

    Add todo comment

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,43 +1,40 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted } from 'vue';
 
-const msg = defineModel("");
+const msg = defineModel('');
 
 function handleFormSubmit() {}
 
 onMounted(() => {
-    const ws = new WebSocket("/chat");
-    ws.onopen = (event) => {
-        console.log(event);
-    };
+  // todo: fix ws connection url -> url is being parsed incorrectly
+  const ws = new WebSocket('/chat');
+  ws.onopen = (event) => {
+    console.log(event);
+  };
 });
 </script>
 
 <template>
-    <main>
-        <h1>Hello, Chat App!</h1>
-        <section class="chat_window">
-            <ul></ul>
-        </section>
-        <section class="chat_box">
-            <form @submit.prevent="handleFormSubmit">
-                <input
-                    type="text"
-                    placeholder="Start typing here..."
-                    v-model="msg"
-                />
-                <input type="submit" value="Send" />
-            </form>
-        </section>
-    </main>
+  <main>
+    <h1>Hello, Chat App!</h1>
+    <section class="chat_window">
+      <ul></ul>
+    </section>
+    <section class="chat_box">
+      <form @submit.prevent="handleFormSubmit">
+        <input type="text" placeholder="Start typing here..." v-model="msg" />
+        <input type="submit" value="Send" />
+      </form>
+    </section>
+  </main>
 </template>
 <style scoped>
 .chat_window {
-    border: var(--border-style);
-    border-radius: var(--border-radius);
-    padding: var(--spacing-00);
-    width: 100%;
-    height: 60vh;
-    overflow-x: scroll;
+  border: var(--border-style);
+  border-radius: var(--border-radius);
+  padding: var(--spacing-00);
+  width: 100%;
+  height: 60vh;
+  overflow-x: scroll;
 }
 </style>
